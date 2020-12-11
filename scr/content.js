ccd64607230595a783148c61ec3ed256b49ffc80
La7rodectus <maxmega27@gmail.com>
2020-12-11 16:14:50 +0200

    file struct fix

--- scr/content.js
+++ scr/content.js
@@ -1,41 +1,49 @@
 'use strict';
 /* eslint-disable max-len */
 /* eslint-disable no-undef */
-
+const debag = true;
 
 
 // library
 function logHref() {
   const windowHref = document.location.href;
   console.log(windowHref);
-  return windowHref;
 }
 
-function findInFramesRec(selector, doc) {
+function sendRuntimeMessage(msg) {
+  try {
+    chrome.runtime.sendMessage(msg);
+  } catch (err) {
+    if (debug) throw new Error(err);
+  }
+}
+
+function findInFramesSelector(selector, doc) {
   try {
     let hit = doc.querySelector(selector);
     if (hit) return hit;
     const frames = Array.prototype.slice.call(doc.frames);
-    for (let i = 0; (i < frames.length) &&   !hit; i++) {
+    for (let i = 0; (i < frames.length) && !hit; i++) {
       try {
         if (!frames[i] || !frames[i].document) continue;
-        hit = findInFramesRec(selector, frames[i].document);
+        hit = findInFramesSelector(selector, frames[i].document);
       } catch (err) {
-        console.warn('Can\'t fined video element ' + err.name + ': ' + err.message);
+        console.warn('Can\'t fined video element error-> ' + err.name + ': ' + err.message);
       }
     }
     return hit;
   } catch (err) {
-    console.warn('Can\'t exec inFrame search ' + err.name + ': ' + err.message);
+    console.warn('Can\'t exec inFrame search error-> ' + err.name + ': ' + err.message);
     return false;
   }
 
 }
 
+//Listeners
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
   if (request.message === 'clicked_browser_action') {
-    const res = findInFramesRec('video', document);
+    const res = findInFramesSelector('video', document);
     if (res) {
       res.pause();
     }
